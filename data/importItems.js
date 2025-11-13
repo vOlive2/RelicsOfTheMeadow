f2a3155b103163906d35380999eb05699100d627
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-11-13 11:05:03 -0700

    Create importItems.js

---
+++ data/importItems.js
@@ -0,0 +1,13 @@
+export const importItems = [
+  {
+    name: "Cookies",
+    price: 3,
+    statBoosts: {happiness: 2},
+  },
+  {
+    name: "Swords",
+    price: 10,
+    statBoosts: {troops: 1},
+  }
+];
+console.log("✅ Imports are loaded", imports);

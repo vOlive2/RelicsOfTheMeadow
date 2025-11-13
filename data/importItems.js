3109a63d45b2f9c75b9410aaae9a9ec545853060
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-11-13 11:11:41 -0700

    Update importItems.js

--- data/importItems.js
+++ data/importItems.js
@@ -8,6 +8,16 @@ export const importItems = [
     name: "Swords",
     price: 10,
     statBoosts: {troops: 1},
+  },
+  {
+    name: "Energy Drink",
+    price: 3,
+    statBoosts: {happiness: 1, energy: 5},
+  },
+  {
+    name: "Raw gold",
+    price: 50,
+    statBoosts: {gold: 10},
   }
 ];
 console.log("✅ Imports are loaded", imports);
